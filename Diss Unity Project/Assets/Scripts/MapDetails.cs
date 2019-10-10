fd55ed3bac0d4d10d28f782ba455b71ffb2f0c75
Sophie Traynor <traynorsl@cardiff.ac.uk>
2019-10-10 16:03:41 +0100

    Revert "removed examples folder"
    This reverts commit c41e6b5d80f99765437fe12c087e0a8985520ea9.

--- Diss Unity Project/Assets/Scripts/MapDetails.cs
+++ Diss Unity Project/Assets/Scripts/MapDetails.cs
@@ -8,21 +8,20 @@ using Newtonsoft.Json.Linq;
 //https://github.com/Placenote/PlacenoteSDK-Unity/blob/master/Assets/Placenote/Examples/RandomShapes/MapInfoElement.cs
 public class MapDetails : MonoBehaviour
 {
-    [SerializeField] Text mMapIdText;
-    [SerializeField] Toggle mToggle;
+	[SerializeField] Text routeIdText;
+	[SerializeField] Toggle toggle;
 
-    public void Initialize(LibPlacenote.MapInfo mapInfo, ToggleGroup toggleGroup,
-                           RectTransform listParent, UnityAction<bool> onToggleChanged)
-    {
-        mMapIdText.text = mapInfo.placeId;
-        if (mapInfo.metadata.name != null && mapInfo.metadata.name.Length > 0)
-        {
-            mMapIdText.text = mapInfo.metadata.name;
-        }
-        mToggle.group = toggleGroup;
-        gameObject.transform.SetParent(listParent);
-        mToggle.onValueChanged.AddListener(onToggleChanged);
+	public void Initialize (LibPlacenote.MapInfo routeInfo, ToggleGroup toggleGroup,
+	                       RectTransform listParent, UnityAction<bool> onToggleChanged)
+	{
+		routeIdText.text = routeInfo.placeId;
+		if (routeInfo.metadata.name != null && routeInfo.metadata.name.Length > 0) {
+			routeIdText.text = routeInfo.metadata.name;
+		}
+		toggle.group = toggleGroup;
+		gameObject.transform.SetParent (listParent);
+		toggle.onValueChanged.AddListener (onToggleChanged);
 
-    }
+	}
 
 }
